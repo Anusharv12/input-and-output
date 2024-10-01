e1b0d3822112fc9d93ebc8ae4269aaa3d9ed8ed7
Anusharv <anusharathna512@gmail.com>
2024-10-01 18:21:40 +0530

    Update DhoniAge.py

--- DhoniAge.py
+++ DhoniAge.py
@@ -28,54 +28,7 @@ p=input("Banerjee's Question:\n")
 r=input("Dhoni's Reply:\n")
 print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p,r))
 
-'''3)3)It was in the 1997-98 season that Ranchi saw the rise of the Captain Cool in the interschool trophy between DAV Jawahar Vidhya Mandir and Kendriya School. It was in that match Dhoni convinced Banerjee to be the opener and justified it with a double century.
-Write a program to display the details of the match with Team name, Scores of the team and Overs played.
-Input and Output Format:  
-Refer sample input and output for formatting specifications.
-[All text in bold corresponds to input and the rest corresponds to output]
-Sample Input and Output:
-Team 1:
-Team Name:
-DAV Jawahar Vidhya Mandir
-Score:
-150
-Overs played:
-20
-Team 2:
-Team name:
-Kendriya School
-Score:
-110
-Overs played:
-18
-Match Details:
-Team 1:
-Name: DAV Jawahar Vidhya Mandir
-Score: 150
-Overs played: 20
-Team 2:
-Name:  Kendriya School
-Score: 110
-Overs played: 18'''
 
-Ans:
-    print("Team 1:")
-    team1_name = input("Team Name:\n")
-    team1_score = input("Score:\n")
-    team1_overs = input("Overs played:\n")
-    print("Team 2:")
-    team2_name = input("Team name:\n")
-    team2_score = input("Score:\n")
-    team2_overs = input("Overs played:\n")
-    print("Match Details:")
-    print(f"Team 1:")
-    print(f"Name: {team1_name}")
-    print(f"Score: {team1_score}")
-    print(f"Overs played: {team1_overs}")
-    print(f"Team 2:")
-    print(f"Name: {team2_name}")
-    print(f"Score: {team2_score}")
-    print(f"Overs played: {team2_overs}")
 
 
 
